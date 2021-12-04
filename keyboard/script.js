b728ba74ec496c1e147b7bb4220d4384b0989ae0
goyal <goyalhimanshu079@gmail.com>
2021-12-04 14:31:53 +0530

    return key functional

--- keyboard/script.js
+++ keyboard/script.js
@@ -1,10 +1,13 @@
 const content = document.getElementById("screen");
 let txt = "";
 function erase() {
-  txt = txt.split("").slice(0, -1).join("");
+  txt = txt.slice(0, -1);
   content.innerText = txt;
 }
 function pressBtn(btn) {
   txt += btn;
   content.innerText = txt;
+}
+function enter() {
+  txt += "\n";
 }
