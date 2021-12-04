93dfd66b4f1fc4dfbd15109ea25391d1c6485ad8
goyal <goyalhimanshu079@gmail.com>
2021-12-04 14:26:29 +0530

    backspace working

--- keyboard/script.js
+++ keyboard/script.js
@@ -1,6 +1,9 @@
 const content = document.getElementById("screen");
 let txt = "";
-
+function erase() {
+  txt = txt.split("").slice(0, -1).join("");
+  content.innerText = txt;
+}
 function pressBtn(btn) {
   txt += btn;
   content.innerText = txt;
