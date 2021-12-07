8eb5fdf5628e3e863b9de5859b5fce0e391bd234
goyal <goyalhimanshu079@gmail.com>
2021-12-07 19:15:05 +0530

    local storage synced and active

--- shopping cart/cart.js
+++ shopping cart/cart.js
@@ -13,6 +13,28 @@ cart.addEventListener("click", () => {
   cartPage.classList.toggle("hide");
 });
 // add to cart
+const vegetables = {
+  Tomato: {
+    name: "Tomato",
+    price: "1.2$/kg",
+  },
+  Cucumber: {
+    name: "Cucumber",
+    price: "2$/kg",
+  },
+  Eggs: {
+    name: "Eggs",
+    price: "3$/dz",
+  },
+  CoffeeBeans: {
+    name: "Coffee Beans",
+    price: "3.6$/kg",
+  },
+  GreenPea: {
+    name: "Green Pea",
+    price: "0.8$/kg",
+  },
+};
 const tableBody = document.getElementById("cart-items");
 function reconstruct() {
   for (vege of Object.keys(localStorage)) {
@@ -22,12 +44,13 @@ function reconstruct() {
     let itemQty = document.createElement("td");
     let itemTotal = document.createElement("td");
     itemRow.setAttribute("id", vege);
-    itemName.innerText = vege;
-    const item = document.getElementById(vege);
-    itemPrice.innerText = item.children[2].children[0].children[1].innerText;
+    itemName.innerText = vegetables[vege].name;
+    itemPrice.innerText = vegetables[vege].price;
     itemQty.innerText = localStorage.getItem(vege);
     itemTotal.innerText =
-      Number(itemPrice.innerText.split("$")[0]) * itemQty.innerText + "$";
+      (Number(itemPrice.innerText.split("$")[0]) * itemQty.innerText).toFixed(
+        2
+      ) + "$";
     itemRow.appendChild(itemName);
     itemRow.appendChild(itemPrice);
     itemRow.appendChild(itemQty);
@@ -37,28 +60,45 @@ function reconstruct() {
 }
 reconstruct();
 function addToCart(item) {
-  let itemRow = document.createElement("tr");
-  let itemName = document.createElement("td");
-  let itemPrice = document.createElement("td");
-  let itemQty = document.createElement("td");
-  let itemTotal = document.createElement("td");
-  itemName.innerText = item.children[2].children[0].children[0].innerText;
-  const vege = itemName.innerText;
-  itemRow.setAttribute("id", vege);
-  itemPrice.innerText = item.children[2].children[0].children[1].innerText;
-  itemQty.innerText = 1;
-  const quant = itemQty.innerText;
-  localStorage.setItem(vege, quant);
-  itemTotal.innerText =
-    Number(itemPrice.innerText.split("$")[0]) * itemQty.innerText + "$";
-  itemRow.appendChild(itemName);
-  itemRow.appendChild(itemPrice);
-  itemRow.appendChild(itemQty);
-  itemRow.appendChild(itemTotal);
-  tableBody.appendChild(itemRow);
+  let elId = item.getAttribute("id");
+  const vegId = elId.split("-").join("");
+  // console.log(elId);
+  let newTd = document.createElement("td");
+  if (localStorage[vegId]) {
+    let irow = document.getElementById(vegId);
+    let itemPrice = irow.children[1];
+    let itemQty = irow.children[2];
+    let itemTotal = irow.children[3];
+    localStorage[vegId] = Number(localStorage[vegId]) + 1;
+    itemQty.innerText = localStorage[vegId];
+    itemTotal.innerText =
+      (Number(itemPrice.innerText.split("$")[0]) * itemQty.innerText).toFixed(
+        2
+      ) + "$";
+  } else {
+    let itemRow = document.createElement("tr");
+    let itemName = document.createElement("td");
+    let itemPrice = document.createElement("td");
+    let itemQty = document.createElement("td");
+    let itemTotal = document.createElement("td");
+    itemRow.setAttribute("id", vegId);
+    itemName.innerText = vegetables[vegId].name;
+    itemPrice.innerText = vegetables[vegId].price;
+    itemQty.innerText = 1;
+    const quant = itemQty.innerText;
+    localStorage.setItem(vegId, quant);
+    itemTotal.innerText =
+      (Number(itemPrice.innerText.split("$")[0]) * itemQty.innerText).toFixed(
+        2
+      ) + "$";
+    itemRow.appendChild(itemName);
+    itemRow.appendChild(itemPrice);
+    itemRow.appendChild(itemQty);
+    itemRow.appendChild(itemTotal);
+    tableBody.appendChild(itemRow);
+  }
 }
-// const prod = document.getElementById("prod2");
-// addToCart(prod);
+
 let cartIcons = document.querySelectorAll(".fa-cart-plus");
 cartIcons.forEach((icon) => {
   icon.addEventListener("click", (e) => {
