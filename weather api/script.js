842710e81535fdf7f4a8fef4203aa8be3ac0cfb1
goyal <goyalhimanshu079@gmail.com>
2021-12-09 20:58:11 +0530

    day, time of the day, temp(max/min), navbar setup

--- weather api/script.js
+++ weather api/script.js
@@ -1,16 +1,39 @@
-import axios from "axios";
+let loc = document.getElementById("location");
+const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+// let url = `https://api.openweathermap.org/data/2.5/weather?q=surat&appid=a426dd50b8b4887bd4328c35f555359f`;
+// fetch(url)
+//   .then((res) => res.json())
+//   .then((res) => (loc.innerText = res.base));
 
-let data;
-let temp;
-axios
-  .get(
-    "https://api.openweathermap.org/data/2.5/weather?q=Surat&units=metric&appid=a426dd50b8b4887bd4328c35f555359f"
-  )
-  .then((res) => {
-    data = res.data;
-    temp = res.data.main.temp;
-    // console.log(data, temp);
-  })
-  .then(() => {
-    console.log(3, data);
-  });
+let day = document.getElementById("day");
+let time = document.getElementById("time");
+let today = new Date();
+let hours = today.getHours();
+console.log(hours);
+today = weekday[today.getDay()];
+today = today.toUpperCase();
+day.innerText = today;
+function hourSwitch(hour) {
+  switch (true) {
+    case hour > 4 && hour < 12:
+      time.innerText = "MORNING";
+      break;
+    case hour > 12 && hour < 18:
+      time.innerText = "NOON";
+      break;
+    case hour > 18 && hour < 21:
+      time.innerText = "EVENING";
+      break;
+    case hour > 21 || hour < 4:
+      time.innerText = "NIGHT";
+  }
+}
+hourSwitch(hours);
