821efff90e4b6a473c5e2dfd3d2a467ae4d18cc6
goyal <goyalhimanshu079@gmail.com>
2021-12-11 15:22:57 +0530

    search modal css, js done, animation done as well

--- weather api/script.js
+++ weather api/script.js
@@ -6,6 +6,10 @@ const feelsLike = document.getElementById("temp-like");
 const humidity = document.getElementById("humid");
 const windSpeed = document.getElementById("wind");
 const visibility = document.getElementById("visible");
+const city = document.getElementById("city");
+const modal = document.getElementById("modal");
+const btn = document.getElementById("btn");
+const search = document.getElementById("search");
 const weekday = [
   "Sunday",
   "Monday",
@@ -15,41 +19,54 @@ const weekday = [
   "Friday",
   "Saturday",
 ];
-let url = `https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=a426dd50b8b4887bd4328c35f555359f`;
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    let temp = Math.round(data.main.temp);
-    let minTemp = Math.floor(data.main.temp_min);
-    let maxTemp = Math.ceil(data.main.temp_min);
-    let tempLike = Math.round(data.main.feels_like);
-    mainTemp.innerHTML = `${temp}&#176;`;
-    min.innerHTML = `${minTemp}&#176;`;
-    max.innerHTML = `${maxTemp}&#176;`;
-    feelsLike.innerHTML = `${tempLike}&#176;`;
-    return data;
-  })
-  .then((data) => {
-    let humid = data.main.humidity;
-    humidity.innerText = humid + "%";
-    return data;
-  })
-  .then((data) => {
-    let wind = data.wind.speed;
-    windSpeed.innerText = wind + "km/h";
-    return data;
-  })
-  .then((data) => {
-    let visible = data.visibility / 1000;
-    visibility.innerText = visible + "km";
+function changeWeather(city) {
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=a426dd50b8b4887bd4328c35f555359f`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      let temp = Math.round(data.main.temp);
+      let minTemp = Math.floor(data.main.temp_min);
+      let maxTemp = Math.ceil(data.main.temp_min);
+      let tempLike = Math.round(data.main.feels_like);
+      mainTemp.innerHTML = `${temp}&#176;`;
+      min.innerHTML = `${minTemp}&#176;`;
+      max.innerHTML = `${maxTemp}&#176;`;
+      feelsLike.innerHTML = `${tempLike}&#176;`;
+      return data;
+    })
+    .then((data) => {
+      let humid = data.main.humidity;
+      humidity.innerText = humid + "%";
+      return data;
+    })
+    .then((data) => {
+      let wind = data.wind.speed;
+      windSpeed.innerText = wind + "km/h";
+      return data;
+    })
+    .then((data) => {
+      let visible = data.visibility / 1000;
+      console.log(visible);
+      visibility.innerText = visible + "km";
+    });
+}
+search.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+city.addEventListener("input", (e) => {
+  btn.addEventListener("click", () => {
+    let cityName = city.value;
+    loc.innerText = cityName;
+    modal.style.display = "none";
+    changeWeather(cityName);
   });
-
+});
+// Day & Time of the day
 let day = document.getElementById("day");
 let time = document.getElementById("time");
 let today = new Date();
 let hours = today.getHours();
-// console.log(hours);
 today = weekday[today.getDay()];
 today = today.toUpperCase();
 day.innerText = today;
