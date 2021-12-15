d9123476ea9e987629658a4a42a1371cef790387
goyal <goyalhimanshu079@gmail.com>
2021-12-15 14:03:42 +0530

    functionally working

--- mole/app.js
+++ mole/app.js
@@ -8,3 +8,44 @@ function burrow(num) {
   }
 }
 burrow(9);
+
+const square = document.querySelectorAll(".square");
+const mole = document.querySelectorAll(".mole");
+const timeLeft = document.querySelector("#time-left");
+let score = document.querySelector("#score ");
+let seconds = 60;
+let result = 0;
+let timer;
+let smashPos;
+function randomSquare() {
+  square.forEach((sqr) => {
+    sqr.classList.remove("mole");
+  });
+  let randomBox = square[Math.floor(Math.random() * 9)];
+  randomBox.classList.add("mole");
+  //   assigning the id of randomBox to the smashPosition to access
+  smashPos = randomBox.id;
+}
+square.forEach((sqr) => {
+  sqr.addEventListener("mouseup", () => {
+    if (sqr.id === smashPos) {
+      result += 1;
+      score.innerText = result;
+    }
+  });
+});
+
+function moleShift() {
+  timer = setInterval(randomSquare, 500);
+}
+moleShift();
+function CountDown() {
+  seconds--;
+  timeLeft.innerText = seconds;
+  if (seconds == 0) {
+    clearInterval(timer);
+    clearInterval(countInt);
+  }
+}
+
+let countInt = setInterval(CountDown, 1000);
