7e2f306059b944d3c6dd635587c5702e73e2d056
goyal <goyalhimanshu079@gmail.com>
2022-01-04 15:35:42 +0530

    api addresse stored

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -1,42 +1,19 @@
 import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import Screen from "./components/Screen";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [],
+    };
+  }
   render() {
     return (
-      <>
-        <div
-          className="d-flex flex-column flex-shrink-0 p-3 bg-success"
-          style={{ width: " 220px", height: "100vh" }}
-        >
-          <a
-            href="/"
-            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none"
-          >
-            <svg className="bi me-2" width="40" height="32">
-              <use xlinkhref="#bootstrap" />
-            </svg>
-            <span className="fs-4 text-light">Home</span>
-          </a>
-          <hr />
-          <h4 className="ms-2">Categories</h4>
-          <ul className="nav nav-pills mb-auto mt-2 mx-auto">
-            <li className="border-bottom">
-              <a href="/" className="nav-link link-light">
-                Dashboard
-              </a>
-            </li>
-            <li className="border-bottom">
-              <a href="/" className="nav-link link-light">
-                Orders
-              </a>
-            </li>
-            <li className="border-bottom">
-              <a href="/" className="nav-link link-light">
-                Products
-              </a>
-            </li>
-          </ul>
-        </div>
-      </>
+      <div className="d-flex">
+        <NavBar />
+        <Screen />
+      </div>
     );
   }
 }
---
+++ trivia/src/components/Screen.js
@@ -0,0 +1,16 @@
+const Screen = () => {
+  return (
+    <div
+      className="card mx-auto my-auto"
+      style={{ width: "80%", height: "600px" }}
+    >
+      <h3 className="card-header">Quiz?</h3>
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        
+        <div className="btn btn-primary">Go somewhere</div>
+      </div>
+    </div>
+  );
+};
+export default Screen;
